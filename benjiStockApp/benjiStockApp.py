792c9ea42f8edd1894ca4282c2e22f8d8c3d2dfc
nirinium <wills.colton@gmail.com>
2019-03-04 15:37:26 -0500

    init

--- benjiStockApp/benjiStockApp.py
+++ benjiStockApp/benjiStockApp.py
@@ -1,1 +1,23 @@
+import requests
+import time
+import pandas as pd
+#import PySimpleGUI as sg
 
+#def GUI():
+    #sg.PopupYesNo('Is this a popup?')
+
+def mainF():
+    #Request sent for while loop
+    r = requests.get("https://finance.yahoo.com/quote/%5EDJI/components/")
+
+    #While the website request is "OK"
+    while (r.status_code==200):
+	    #Grabs the table data from the website and parses
+	    stocks = pd.read_html("https://finance.yahoo.com/quote/%5EDJI/components/", header=0, parse_dates=["Symbol"])
+	    print(stocks[0])
+	    #Set time for website refresh to update stock numbers
+	    time.sleep(120)
+mainF()
+#Pressing Ctrl + C will stop the script from running
+#GUI()
+exit()
